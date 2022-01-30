5c5e0710167a248b635a8b371d2930c8611f3bcd
MacMini <pierre-macmini@MacMinis-Mac-mini.local>
2022-01-30 12:40:08 -0500

    FAST API yml

--- test/test_handler.py
+++ test/test_handler.py
@@ -2,13 +2,6 @@ import app
 
 
 def test_get_quote():
-    res = app.get_quote()
-
-    assert "author" in res
-    assert "content" in res
-
-
-def test_handler():
-    res = app.handler({}, {})
+    res = app.get_root()
 
     assert "message" in res
